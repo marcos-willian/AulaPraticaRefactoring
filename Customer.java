ad27a22953e08bf3b7e1b61d8f0a461f2bf9eb37
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-03-10 16:06:27 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -23,12 +23,7 @@ public class Customer {
      Enumeration<Rental> rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-    	double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        thisAmount = each.getCharge();
-
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -36,11 +31,11 @@ public class Customer {
             each.getDaysRented() > 1) frequentRenterPoints ++;
 
         //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+        result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
+     
      //add footer lines
      result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
      result += "You earned " + String.valueOf(frequentRenterPoints) +
