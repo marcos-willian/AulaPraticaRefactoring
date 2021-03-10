62a7ce56b2cdfe0c6eec2ba71d11916100f8d3f2
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-03-10 15:58:42 -0300

    Commit 4

--- Rental.java
+++ Rental.java
@@ -15,4 +15,24 @@ public class Rental {
     public Movie getMovie() {
        return _movie;
     }
+    
+    public double getCharge() {
+    	double thisCharge = 0;
+	   	switch (_movie.getPriceCode()) {
+	   	  case Movie.REGULAR:
+	   	     thisCharge += 2;
+	   	     if (_daysRented > 2)
+	   	        thisCharge += (_daysRented - 2) * 1.5;
+	   	     break;
+	   	  case Movie.NEW_RELEASE:
+	   	     thisCharge += _daysRented * 3;
+	   	     break;
+	   	  case Movie.CHILDRENS:
+	   	     thisCharge += 1.5;
+	   	     if (_daysRented > 3)
+	   	        thisCharge += (_daysRented - 3) * 1.5;
+	   	      break;
+	   	 }
+	   	 return thisCharge;
+    }
  }
