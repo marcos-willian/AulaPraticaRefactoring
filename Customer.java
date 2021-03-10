406ce8e17397866736f36e6078f36f77a9dc2bb9
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-03-10 15:22:37 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -47,23 +47,23 @@ public class Customer {
              " frequent renter points";
      return result;
    }
-  private double amountFor(Rental each) {
-	  double thisAmount = 0;
-	  switch (each.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         thisAmount += 2;
-         if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         thisAmount += each.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         thisAmount += 1.5;
-         if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-	  }
-	  return thisAmount;
+  private double amountFor(Rental aRental) {
+	 double thisAmount = 0;
+	 switch (aRental.getMovie().getPriceCode()) {
+	  case Movie.REGULAR:
+	     thisAmount += 2;
+	     if (aRental.getDaysRented() > 2)
+	        thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+	     break;
+	  case Movie.NEW_RELEASE:
+	     thisAmount += aRental.getDaysRented() * 3;
+	     break;
+	  case Movie.CHILDRENS:
+	     thisAmount += 1.5;
+	     if (aRental.getDaysRented() > 3)
+	        thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+	      break;
+	 }
+	 return thisAmount;
   }
 }
