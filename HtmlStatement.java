0aa0565c06bd7b09a920822be8f051f5fba81d34
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-03-11 11:40:45 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,28 +1,13 @@
-import java.util.Enumeration;
 public class HtmlStatement extends Statement {
-	public String value(Customer aCustomer) {
-		 Enumeration<Rental> rentals = aCustomer.getRentals();
-		 String result = printHeader(aCustomer);
-		 while (rentals.hasMoreElements()) {
-		    Rental each = (Rental) rentals.nextElement();
-		    
-		    //show figures for this rental
-		    result += showFigures(each);
-		 }
-		 //add footer lines
-		 result +=  printFooter(aCustomer);
-		 return result;
-	}
-	
-	private String printHeader (Customer aCustomer) {
+	String printHeader (Customer aCustomer) {
 		return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
 	}
 	
-	private String showFigures(Rental thisRental) {
+	String showFigures(Rental thisRental) {
 		return thisRental.getMovie().getTitle() + ": " + String.valueOf(thisRental.getCharge()) + "<BR>\n";
 	}
 	
-	private String printFooter(Customer aCustomer) {
+	String printFooter(Customer aCustomer) {
 		String result = "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
 		result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + 
 	    		 "</EM> frequent renter points<P>";
--- TextStatement.java
+++ TextStatement.java
@@ -1,28 +1,14 @@
-import java.util.Enumeration;
+
 public class TextStatement extends Statement {
-	public String value(Customer aCustomer) {
-		 Enumeration<Rental> rentals = aCustomer.getRentals();
-		 String result = printHeader(aCustomer);
-		 while (rentals.hasMoreElements()) {
-		    Rental each = (Rental) rentals.nextElement();
-		    
-		    //show figures for this rental
-		    result += showFigures(each);
-		 }
-		 //add footer lines
-		 result +=  printFooter(aCustomer);
-		 return result;
-	}
-	
-	private String printHeader (Customer aCustomer) {
+	String printHeader (Customer aCustomer) {
 		return "Rental Record for " + aCustomer.getName() + "\n";
 	}
 	
-	private String showFigures(Rental thisRental) {
+	String showFigures(Rental thisRental) {
 		return  "\t" + thisRental.getMovie().getTitle()+ "\t" + String.valueOf(thisRental.getCharge()) + "\n";
 	}
 	
-	private String printFooter(Customer aCustomer) {
+	String printFooter(Customer aCustomer) {
 		String result = "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
 		 result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
 		         " frequent renter points";
