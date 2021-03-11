3b438347877f155b8e69bd94ee3cdf20aab21d67
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-03-11 11:13:35 -0300

    Commit 16

---
+++ HtmlStatement.java
@@ -0,0 +1,19 @@
+import java.util.Enumeration;
+
+public class HtmlStatement extends Statement {
+	public String value(Customer aCustomer) {
+		 Enumeration<Rental> rentals = aCustomer.getRentals();
+		 String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+		 while (rentals.hasMoreElements()) {
+		    Rental each = (Rental) rentals.nextElement();
+		    
+		    //show figures for this rental
+		    result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		 }
+		 //add footer lines
+		 result +=  "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+	     result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + 
+	    		 "</EM> frequent renter points<P>";
+		 return result;
+	}
+}
